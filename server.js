d093f5dc22c4444118fc7fb2ea2bf21524133ba4
Kevin Sparling <kevinsparling@gmail.com>
2020-02-03 11:42:16 +0800

    refactored to server.js

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const _ = require('lodash')
 
 const app = express()
-var server_port = process.env.SERVER_PORT || 8080
 
 require('./db/mongoose')
 const userRouter = require('./routers/User')
 app.use(express.json())
 app.use('/api', userRouter)
 
-app.listen(server_port, () => {
-  console.log("server running on port " + server_port)
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const server = app.listen(port, () => {
+  console.log("server running on port " + port)
 })
