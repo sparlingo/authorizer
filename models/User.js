5a67a96e5474020b53fdf71f30ad8a52e81393e8
Kevin Sparling <kevinsparling@gmail.com>
2020-02-11 23:41:06 +0800

    updated with cors

--- models/User.js
+++ models/User.js
@@ -4,6 +4,12 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
     email: {
         type: String,
         required: true,
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const _ = require('lodash')
+const cors = require('cors')
 
 const app = express()
+app.use(cors)
+app.use(express.json())
 
 require('./db/mongoose')
 const userRouter = require('./routers/User')
-app.use(express.json())
+
 app.use('/api', userRouter)
 
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 8000;
 const server = app.listen(port, () => {
   console.log("server running on port " + port)
 })
